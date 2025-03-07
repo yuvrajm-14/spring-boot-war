0573558c85282365e353faeecfbc7ef66152271a
Yuvraj Mudliyar <yuvrajmudd44@gmail.com>
2025-03-07 12:45:36 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return " Hello Yuvraj Mudliyar Hello";
+        return " Hello Yuvraj Mudliyar";
     } 
 
 }
